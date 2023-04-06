e17612f6298d7be4fa302a3b2c9d2d40d282922c
clarinar32 <clarismukhwana1@gmail.com>
2023-04-06 01:40:58 -0400

    androidapp

---
+++ app/src/main/java/com/example/loginreg/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.loginreg
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var texttologin:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        texttologin=findViewById(R.id.Texttologin)
+        texttologin.setOnClickListener {
+            val intent=Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
